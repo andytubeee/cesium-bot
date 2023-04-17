a93747d5a5c42ea58413c9311bf92f98b75ca918
Andrew Yang <45147270+andytubeee@users.noreply.github.com>
2023-04-17 14:06:45 -0400

    Update utils.js
    removed comments

--- utils.js
+++ utils.js
@@ -1,17 +1,12 @@
 const getNextCode = (date) => {
   // if date is tuesday, use today's date
   // else, get the next tuesday
-  // get the day and month
-  // use the day and month to generate a random number
-  // return the random number
   const nextTuesday = new Date(date);
   if (nextTuesday.getDay() === 2) {
     // if today is tuesday, use today's date
   } else {
     nextTuesday.setDate(date.getDate() + ((2 + 7 - date.getDay()) % 7));
   }
-
-  // get the next tuesday after the given date
 
   const day = nextTuesday.getDate();
   const month = nextTuesday.getMonth() + 1;
